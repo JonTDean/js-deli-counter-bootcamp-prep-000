f31c59b009a5ff4d78eb966670b69d7716c15658
Jonathan T Dean <jonathantomdean@gmail.com>
2020-01-11 06:58:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 katzDeliLine = [];
 
 function takeANumber(currentLine, newPerson){
- newPerson = "Ada";
+ newPerson = ["Ada", "Grace", "Kent"];
  currentLine= 1;
  return console.log(`Welcome, ${newPerson}. You are number ${currentLine} in line.`);
 }
